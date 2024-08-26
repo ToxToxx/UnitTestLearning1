e8d3f99d5b2799b882af1f0f997cbb89d8033fb3
Matvey <sinema3d00@gmail.com>
2024-08-26 17:58:13 +0300

    made simple test

--- UnitTestLearning1/Program.cs
+++ UnitTestLearning1/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using UnitTestLearning1.UnitTests;
+
+WorldsDumbestFunctionTests.WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString();
--- UnitTestLearning1/UnitTests/WorldsDumbestFunctionTests.cs
+++ UnitTestLearning1/UnitTests/WorldsDumbestFunctionTests.cs
@@ -11,7 +11,29 @@ namespace UnitTestLearning1.UnitTests
         //Naming Convention - Classname_MethodName_ExpectedResult
         public static void WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString()
         {
+            try
+            {
+                //Arrange - take all you need to run testing function
+                int num = 0;
+                WorldsDumbestFunction worldsDumbest = new();
 
+                //Act - execute function
+                string result = worldsDumbest.ReturnsPikachuIfZero(num);
+                
+                //Assert - Whatever is returned - look, is it what you want
+                if(result == "Pikachu")
+                {
+                    Console.WriteLine("PASSED: WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString");
+                }
+                else
+                {
+                    Console.WriteLine("FAILED: WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
