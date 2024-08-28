c9d888440a5152acaef55ceb0866b80ff500e9cd
Matvey <sinema3d00@gmail.com>
2024-08-28 21:24:58 +0300

    Creating test for ping

--- NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NetworkUtility.Ping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace NetworkUtility.Tests.PingTests
 {
     public class NetworkServiceTests
     {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            //Arrange
+
+            //Act
+            var pingService = new NetworkService();
+        }
     }
 }
--- NetworkUtility/Ping/NetworkService.cs
+++ NetworkUtility/Ping/NetworkService.cs
@@ -8,5 +8,9 @@ namespace NetworkUtility.Ping
 {
     public class NetworkService
     {
+        public string SendPing()
+        {
+            return "Success: Ping Sent";
+        }
     }
 }
